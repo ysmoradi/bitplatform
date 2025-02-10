0695772dc30327a98b0079d86a14a4920ca35bd2
ysmoradi <ysmoradi@outlook.com>
2025-02-10 14:41:36 +0100

    test

---
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Components/Pages/Authorize.razor.cs
@@ -0,0 +1,60 @@
+﻿using System.Text.RegularExpressions;
+using Boilerplate.Shared.Controllers.Identity;
+
+namespace Boilerplate.Client.Core.Components.Pages;
+
+// Opening https://boilerplate-based-project.com/authorize?client_id=NopCommerceClient1&redirect_uri=https://my-nop-commerce-website.com/login&State=carts
+// Will open https://my-nop-commerce-website.com/login#access_token=di1d98cxh913fh29ufhnfunxw9&expires_in=3600&state=carts
+// Local test url: http://localhost:5030/authorize?client_id=NopClient&redirect_uri=https://my-nop-commerce-website.com/login&state=carts
+
+public partial class Authorize
+{
+    [AutoInject] private IUserController userController = default!;
+    [AutoInject] private IAuthTokenProvider authTokenProvider = default!;
+
+    [Parameter, SupplyParameterFromQuery(Name = "client_id")] public string? ClientId { get; set; }
+    [Parameter, SupplyParameterFromQuery(Name = "redirect_uri")] public string? RedirectUri { get; set; }
+    [Parameter, SupplyParameterFromQuery(Name = "state")] public string? State { get; set; }
+
+    private Dictionary<string, string[]> clients = new() // You can also fetch clients from server api
+    {
+        {
+            "NopClient",
+            [
+                "https://my-nop-commerce-website.com/login",
+                "https://localhost:5030/BitAuth/LoginCallback",
+                "https://demo-sso.azurewebsites.net/BitAuth/LoginCallback",
+                "https://nopclient.azurewebsites.net/BitAuth/LoginCallback",
+            ]
+        },
+    };
+
+    protected override async Task OnAfterFirstRenderAsync()
+    {
+        if (clients.TryGetValue(ClientId!, out var clientAllowedRedirectUrls) is false)
+        {
+            NavigationManager.NavigateTo($"{RedirectUri}#error=Invalid or missing client_id&state={State}");
+            return;
+        }
+        Uri uri = new Uri(RedirectUri!);
+        string cleanReturnUrl = uri.GetLeftPart(UriPartial.Path);
+        string normalizedUrl = Regex.Replace(cleanReturnUrl, "(?<!:)/{2,}", "/");
+        if (clientAllowedRedirectUrls.Any(clientUrl => string.Equals(clientUrl, normalizedUrl, StringComparison.InvariantCultureIgnoreCase)) is false)
+        {
+            NavigationManager.NavigateTo($"{RedirectUri}#error=Invalid redirect uri&state={State}");
+            return;
+        }
+
+        _ = await userController.GetCurrentUser(CurrentCancellationToken); // Make sure the current user session still valid.
+
+        var accessToken = await authTokenProvider.GetAccessToken();
+
+        var token = IAuthTokenProvider.ParseAccessToken(accessToken, validateExpiry: true);
+
+        var expiresIn = long.Parse(token.FindFirst("exp")!.Value) - long.Parse(token.FindFirst("iat")!.Value);
+
+        NavigationManager.NavigateTo($"{normalizedUrl}?access_token={accessToken}&token_type=Bearer&expires_in={expiresIn}&state={State}");
+
+        await base.OnAfterFirstRenderAsync();
+    }
+}
--- src/Templates/Boilerplate/Bit.Boilerplate/src/Server/Boilerplate.Server.Web/Program.Middlewares.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Server/Boilerplate.Server.Web/Program.Middlewares.cs
@@ -299,7 +299,7 @@ public static partial class Program
                     var qs = HttpUtility.ParseQueryString(httpContext.Request.QueryString.Value ?? string.Empty);
                     qs.Remove("try_refreshing_token");
                     var returnUrl = UriHelper.BuildRelative(httpContext.Request.PathBase, httpContext.Request.Path, new QueryString($"?{qs}"));
-                    httpContext.Response.Redirect($"{Urls.NotAuthorizedPage}?return-url={returnUrl}&isForbidden={(is403 ? "true" : "false")}");
+                    httpContext.Response.Redirect($"{Urls.NotAuthorizedPage}?return-url={Uri.EscapeDataString(returnUrl)}&isForbidden={(is403 ? "true" : "false")}");
                 }
                 else if (httpContext.Response.StatusCode is 404 &&
                     httpContext.GetEndpoint() is null /* Please be aware that certain endpoints, particularly those associated with web API actions, may intentionally return a 404 error. */)
